e3beb155358084f74926d7f13a533f61b38d009d
tadahiroueta <tadahiroueta@gmail.com>
2023-10-02 09:57:57 -0500

    Initial Commit

---
+++ magic-8-ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic-8-ball
+//
+//  Created by Ueta, Lucas T on 10/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
